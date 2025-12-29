d4a0fcc39b986c7fc1abc446a7fe8e1c1fab1c9a
akip1101 <ayatamu1101@gmail.com>
2025-12-29 22:31:44 +0900

    update

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({
+  ws: { intents: ["GUILDS", "GUILD_MESSAGES"] }
+});
+
+const PREFIX = "!mc";
 
 client.once("ready", () => {
   console.log(`ログイン成功: ${client.user.tag}`);
@@ -7,9 +11,42 @@ client.once("ready", () => {
 
 client.on("message", message => {
   if (message.author.bot) return;
-  if (message.content === "ping") {
-    message.channel.send("pong");
+  if (!message.content.startsWith(PREFIX)) return;
+
+  const args = message.content.split(" ");
+  const command = args[1];
+
+  // !mc start <ip>
+  if (command === "start") {
+    const ip = args[2];
+    if (!ip) {
+      return message.channel.send("❌ IPアドレスを指定してください\n例: `!mc start play.example.com`");
+    }
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle("🟢 マインクラフトサーバー起動")
+      .setColor("#00ff88")
+      .addField("IPアドレス", `\`${ip}\``)
+      .addField("起動者", message.author.tag)
+      .setTimestamp();
+
+    message.channel.send(embed);
+  }
+
+  // !mc stop
+  if (command === "stop") {
+    const embed = new Discord.MessageEmbed()
+      .setTitle("🔴 マインクラフトサーバー停止")
+      .setColor("#ff4444")
+      .addField("停止者", message.author.tag)
+      .setTimestamp();
+
+    message.channel.send(embed);
   }
 });
+
+// エラーでBotが落ちるのを防ぐ
+process.on("unhandledRejection", console.error);
+process.on("uncaughtException", console.error);
 
 client.login(process.env.DISCORD_TOKEN);
